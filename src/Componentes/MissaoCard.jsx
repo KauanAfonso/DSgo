230e7b8cf6b125ad2808705ad5d36f58603a7987
kauanAfonso <kauanafon3@gmail.com>
2025-10-10 09:07:08 -0300

    Tema para dragon ball

--- src/Componentes/MissaoCard.jsx
+++ src/Componentes/MissaoCard.jsx
@@ -1,9 +1,27 @@
-export function MissaoCard({ missao, onIniciarMissao,concluida  }) {
+export function MissaoCard({ missao, onIniciarMissao, concluida }) {
+  function handleClick() {
+    onIniciarMissao(missao);
+  }
+
   return (
-    <article className='missao-card'>
+    <article
+      className='missao-card'
+      id="article_card"
+      style={{ backgroundColor: concluida ? "lightgray" : "white" }}
+    >
       <h3 id={missao.id}>{missao.titulo}</h3>
       <p>{missao.missao}</p>
-      <button onClick={() => onIniciarMissao(missao)}  disabled={concluida}>{concluida ? "Missão concluída" : "Iniciar Missão"}</button>
+      <button
+        onClick={handleClick}
+        disabled={concluida}
+        style={{
+          backgroundColor: concluida ? "gray" : "#007bff",
+          color: "white",
+          cursor: concluida ? "not-allowed" : "pointer",
+        }}
+      >
+        {concluida ? "Missão concluída" : "Iniciar Missão"}
+      </button>
     </article>
-  )
+  );
 }
